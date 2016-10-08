194bef39148be0e53c2ae8734a533697309bcffe
Ed Powderham <edpowderham@outlook.com>
2016-10-08 09:18:06 +0100

    Recreation of takeaway class + related tests

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,15 +1,20 @@
+class Takeaway
 
-@dishes = [
-  {name: "Cheese & Tomato", price:  14.99},
-  {name: "Mighty Meaty", price:  14.99},
-  {name: "Pepperoni Passion", price:  16.99},
-  {name: "Texas BBQ", price:  17.99},
-  {name: "Vegi Supreme", price:  15.99},
-  {name: "American Hot", price:  18.99},
-  {name: "Chicken Feast", price:  17.99},
-  {name: "Hawaiian", price:  18.99},
-  {name: "Meateor", price:  18.99},
-]
+attr_reader :dishes
+
+def initialize
+  @dishes = [
+    {name: "Cheese & Tomato", price:  14.99},
+    {name: "Mighty Meaty", price:  14.99},
+    {name: "Pepperoni Passion", price:  16.99},
+    {name: "Texas BBQ", price:  17.99},
+    {name: "Vegi Supreme", price:  15.99},
+    {name: "American Hot", price:  18.99},
+    {name: "Chicken Feast", price:  17.99},
+    {name: "Hawaiian", price:  18.99},
+    {name: "Meateor", price:  18.99},
+  ]
+end
 
 @current_order = []
 
@@ -56,70 +61,70 @@ def view_menu
   puts
 end
 
-def select_order
-  puts "Please choose your order"
-  puts "Select 0 to view menu"
-  puts "Select pizza by number"
-  puts "Type anything else to return to menu"
-  order(gets.chomp)
+# def select_order
+#   puts "Please choose your order"
+#   puts "Select 0 to view menu"
+#   puts "Select pizza by number"
+#   puts "Type anything else to return to menu"
+#   order(gets.chomp)
+# end
+
+# def order(selection)
+#   puts "Your selection: #{@dishes[selection.to_i-1][:name]}"
+#   puts "Select another pizza by its number (0 to view menu)"
+#   puts "Type anything else to return to menu"
+#
+#   case selection
+#     when "0"
+#       view_menu
+#     when "1"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "2"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "3"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "4"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "5"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "6"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "7"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "8"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     when "9"
+#       food = @dishes[selection.to_i-1]
+#       @current_order << food
+#       order(gets.chomp)
+#     else
+#       puts "Returning to menu"
+#       order(gets.chomp)
+#     end
+# end
+
+# def print_current_order
+#   # output = Hash.new 0
+#   # @current_order.each do |order|
+#   #   output[order] += 1
+#   # end
+#   print @current_order.map{|x| x[:price]}
+# end
+
 end
-
-def order(selection)
-  puts "Your selection: #{@dishes[selection.to_i-1][:name]}"
-  puts "Select another pizza by its number (0 to view menu)"
-  puts "Type anything else to return to menu"
-
-  case selection
-    when "0"
-      view_menu
-    when "1"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "2"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "3"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "4"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "5"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "6"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "7"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "8"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    when "9"
-      food = @dishes[selection.to_i-1]
-      @current_order << food
-      order(gets.chomp)
-    else
-      puts "Returning to menu"
-      order(gets.chomp)
-    end
-end
-
-def print_current_order
-  output = Hash.new 0
-  @current_order.each do |order|
-    output[order] += 1
-  end
-  print output
-end
-
-interactive_menu
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -8,8 +8,21 @@ describe Takeaway do
       expect(takeaway.dishes.length).to be > 1
     end
 
-    it "view_menu" do
-
+    it "Dishes should have a name" do
+      expect(takeaway.dishes.last[:name]).not_to be nil
     end
+
+    it "Dishes should have a price" do
+      expect(takeaway.dishes.last[:price]).not_to be nil
+    end
+  end
+
+  context "#view_menu" do
+
+    it "Menu can be printed" do
+      {takeaway.dishes.length + 2}.times do expect(STDOUT).to receive(:puts) end
+      takeaway.view_menu
+    end
+
   end
 end
